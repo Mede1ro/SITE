5364cdfb4f06fd6711b142c14832ab9cf3c68e15
Mede1ros <mede1ro@proton.me>
2023-12-03 01:53:29 -0300

    V1.4 + Dashboard (Colocar Imagens Nas Pastas)

---
+++ Pages/Dashboard.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Css/Style.css">
+    <link rel="shortcut icon" href="../cocaina.png" type="image/x-icon">
+    <title>SosaBank</title>
+</head>
+
+<body>
+    <div class="container-dashboard">
+        <div class="conteudo-dashboard-menu">
+            <div class="conteudo-dashboard-menu-titulo">
+                <h2>Sosa Admin</h2>
+            </div>
+            <div class="conteudo-dashboard-menu-itens">
+                <ul class="conteudo-dashboard-menu-item">
+                    <li class="dashboard-item-link"><a href=""><img src="../dashboard.png" alt="">DASHBOARD</a></li>
+                    <li class="dashboard-item-link"><a href=""> <img src="../group.png" alt=""> USUÁRIOS</a></li>
+                    <li class="dashboard-item-link"><a href=""> <img src="../balance.png" alt=""> CONTAS</a></li>
+                    <li class="dashboard-item-link"><a href=""> <img src="../transfer.png" alt=""> TRANSAÇÕES</a></li>
+                    <li class="dashboard-item-link"><a href=""> <img src="../active.png" alt=""> NOTIFICAÇÕES</a></li>
+                    <li class="dashboard-item-link deslogar"><a href=""> <img src="../reject.png" alt=""> DESLOGAR</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="conteudo-dashboard">
+            <div class="conteudo-dashboard-card">
+                <div class="conteudo-dashboard-card titulo">
+                    <h1>DASHBOARD | <span>ANÁLISE DE DADOS</span></h1>
+                </div>
+                <div class="conteudo-dashboard-card-dados">
+                    <div class="cards">
+                        <img src="../silhueta-do-grupo-de-usuarios (1).png" alt="">
+                        <h2>12.500</h2>
+                        <p>FUNCIONÁRIOS ATIVOS</p>
+                    </div>
+                    <div class="cards">
+                        <img src="../botao-mais.png" alt="">
+                        <h2>78.295</h2>
+                        <p>CONTAS NOVAS</p>
+                    </div>
+                    <div class="cards">
+                        <img src="../botao-de-menos.png" alt="">
+                        <h2>10.324</h2>
+                        <p>CONTAS CANCELADAS</p>
+                    </div>
+                    <div class="cards">
+                        <img src="../dolar.png" alt="">
+                        <h2>R$ 593.239.473</h2>
+                        <p>MONTANTE TOTAL</p>
+                    </div>
+                </div>
+                <div class="conteudo-dashboard-card-loading">
+                    <div class="lds-ring">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <p>Carregando Dados do Sistema</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- Redirecionamento/Validacao.php
+++ Redirecionamento/Validacao.php
@@ -3,9 +3,14 @@
 $user = $_POST["user"];
 $pass = $_POST["pass"];
 
-if (!empty($user) and !empty($pass)) {
+if ($user == "Igor" and $pass == "123") {
+    header("Location: ../Pages/Dashboard.php");
+} elseif ($user == "Luiz" and $pass == "123") {
+    header("Location: ../Pages/Registro.php");
+} elseif (!empty($user) and !empty($pass)) {
     header("Location: ../Index.php");
 } else {
     print("Nome/Senha inválidos!");
 }
+
 ?>
